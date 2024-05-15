ee0de2fab038c29a72d63dcaf8b955541de18244
Hardtmuth <abramow-a.v@ya.ru>
2024-05-15 23:20:09 +0300

    add eslint

--- trees/itinerary/itinerary.js
+++ trees/itinerary/itinerary.js
@@ -1,4 +1,4 @@
-import _ from 'lodash'
+import _ from 'lodash';
 
-const testLodash = _.add(100, 11)
-console.log(testLodash) //111
+const testLodash = _.add(100, 11);
+console.log(testLodash); // 111
